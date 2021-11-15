f59e67a40520746b97ee7d2c74ddccaa8cb71d81
Vlad-Pascu <pascuvlad2001@yahoo.com>
2021-11-15 08:43:19 +0200

    CarDetails + UserDetails

---
+++ src/main/java/com/park/parkinglot/common/UserDetails.java
@@ -0,0 +1,38 @@
+package com.park.parkinglot.common;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class UserDetails implements java.io.Serializable {
+    private Integer id;
+    private String username;
+    private  String email;
+    private String position;
+
+    public UserDetails(Integer id, String username, String email, String position) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.position = position;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+    
+}
